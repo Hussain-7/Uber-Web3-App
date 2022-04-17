0af158dd469be85bf812f6d4f13067049e797cf2
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2022-04-18 01:44:29 +0500

    assets for ride selector added

--- components/Confirm.js
+++ components/Confirm.js
@@ -1,7 +1,32 @@
 import React from "react"
+import RideSelector from "./RideSelector"
+
+const style = {
+  wrapper: `flex-1 h-full flex flex-col justify-between`,
+  rideSelectorContainer: `h-full flex flex-col overflow-scroll`,
+  confirmButtonContainer: ` border-t-2 cursor-pointer z-10`,
+  confirmButton: `bg-black text-white m-4 py-2 text-center text-xl`,
+}
 
 const Confirm = () => {
-  return <div>Confirm</div>
+  const storeTripDetails = () => {}
+  return (
+    <div className={style.wrapper}>
+      <div className={style.rideSelectorContainer}>
+        {pickupCoordinates && dropoffCoordinates && <RideSelector />}
+      </div>
+      <div className={style.confirmButtonContainer}>
+        <div className={style.confirmButtonContainer}>
+          <div
+            className={style.confirmButton}
+            onClick={() => storeTripDetails(pickup, dropoff)}
+          >
+            Confirm UberX
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Confirm
---
+++ components/RideSelector.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RideSelector = () => {
+	return (
+		<div>RideSelector</div>
+	)
+}
+
+export default RideSelector
