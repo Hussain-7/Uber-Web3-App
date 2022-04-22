44e9d1e65339cc3f4b23aae61480806274b33e1a
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2022-04-23 01:18:00 +0500

    map logic updated for autoadjusment on locations

--- components/Map.js
+++ components/Map.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef } from "react"
+import { useEffect, useContext } from "react"
 import mapboxgl from "mapbox-gl"
 import { UberContext } from "../context/uberContext"
 
@@ -10,43 +10,35 @@ mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
 
 const Map = () => {
   const { pickupCoordinates, dropoffCoordinates } = useContext(UberContext)
-  console.log(pickupCoordinates, dropoffCoordinates)
-  const mapContainer = useRef(null)
-  const map = useRef(null)
 
   useEffect(() => {
-    if (map.current) return // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
+    const map = new mapboxgl.Map({
+      container: "map",
       style: "mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph",
       center: [74.329376, 31.58], //Lahore long lang
       zoom: 4,
     })
-  }, [])
-  useEffect(() => {
+
     if (pickupCoordinates) {
       addToMap(map, pickupCoordinates)
     }
+
     if (dropoffCoordinates) {
       addToMap(map, dropoffCoordinates)
     }
+
     if (pickupCoordinates && dropoffCoordinates) {
-      map.current.fitBounds([dropoffCoordinates, pickupCoordinates], {
-        padding: 400,
+      map.fitBounds([dropoffCoordinates, pickupCoordinates], {
+        padding: 100,
       })
     }
   }, [pickupCoordinates, dropoffCoordinates])
 
   const addToMap = (map, coordinates) => {
-    try {
-      const marker1 = new mapboxgl.Marker()
-        .setLngLat(coordinates)
-        .addTo(map.current)
-    } catch (e) {
-      console.log("error in addToMap: ")
-    }
+    const marker1 = new mapboxgl.Marker().setLngLat(coordinates).addTo(map)
   }
-  return <div ref={mapContainer} className={style.wrapper} />
+
+  return <div className={style.wrapper} id="map" />
 }
 
 export default Map
